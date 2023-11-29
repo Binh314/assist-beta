b4d2a87054135ef6c93c9bdab72d87cc74b8cedc
Angelina Zheng <ajz@mit.edu>
2023-11-28 22:18:44 -0500

    Update homepage to have more functionality in layout (images and text not finalized)

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -1,18 +1,46 @@
 <script setup lang="ts">
+import router from "@/router";
 import { useUserStore } from "@/stores/user";
 import { storeToRefs } from "pinia";
 
 const { currentUsername, isLoggedIn } = storeToRefs(useUserStore());
+
+function login() {
+  void router.push({ name: "Login" });
+}
+
+async function signup() {
+  void router.push({ name: "Register" });
+}
 </script>
 
 <template>
   <main>
     <section>
       <h1 v-if="isLoggedIn">Welcome {{ currentUsername }}!</h1>
-      <div class="content">
+      <div class="content" id="content-1">
+        <img src="@/assets/images/homepage.png" />
+        <div class="text">^ placeholder image; insert blurb that conveys value of using Assist</div>
+        <a href="#content-2">
+          <img class="icon" src="@/assets/images/scroll_down.png" />
+        </a>
+      </div>
+      <div class="content" id="content-2">
+        <img src="@/assets/images/homepage_help.png" />
+        <div class="text">^ placeholder image; insert blurb that conveys value of using Assist</div>
+        <a href="#content-3">
+          <img class="icon" id="content-2-icon" src="@/assets/images/scroll_down.png" />
+        </a>
+      </div>
+      <div class="content" id="content-3">
         <img src="@/assets/images/temp_logo.png" />
-        <div class="text">^ placeholder image (replace with screenshot of part of app or maybe storyboard to give a sense of how the app works); insert blurb on aims of Assist</div>
-        <img class="icon" src="@/assets/images/scroll_down.png" />
+        <div>
+          <button @click="signup">Sign up</button>
+          <button @click="login">Login</button>
+        </div>
+        <a href="#">
+          <img class="icon" id="content-3-icon" src="@/assets/images/top_of_page.png" />
+        </a>
       </div>
     </section>
   </main>
@@ -25,15 +53,32 @@ h1 {
 
 .content {
   display: block;
-  margin-top: 3em;
+  padding-top: 2em;
   text-align: center;
   width: 100%;
   align-items: center;
   justify-content: center;
 }
 
+#content-2 {
+  padding-top: 10em;
+}
+
+#content-2-icon {
+  padding-top: 5em;
+}
+
+#content-3 {
+  padding-top: 5em;
+}
+
+#content-3-icon {
+  padding-top: 1em;
+  padding-bottom: 2em;
+}
+
 img {
-  max-width: 40%;
+  max-width: 45%;
 }
 
 .text {
@@ -49,5 +94,11 @@ img {
 
 .icon:hover {
   cursor: pointer;
+}
+
+button {
+  margin-left: 2em;
+  margin-right: 2em;
+  margin-top: 1em;
 }
 </style>
