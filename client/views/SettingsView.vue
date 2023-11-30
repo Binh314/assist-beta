d4e46daba4aad1debb17bddb9538e251f47cbb54
PapaFruta <8912yl04@gmail.com>
2023-11-29 19:07:36 -0500

    finished setting

--- client/views/SettingsView.vue
+++ client/views/SettingsView.vue
@@ -4,6 +4,7 @@ import ChangePassword from "../components/Setting/ChangePassword.vue";
 import ChangeUsername from '../components/Setting/ChangeUsername.vue';
 import DeleteAccount from '../components/Setting/DeleteAccount.vue';
 import LogOut from '../components/Setting/LogOut.vue';
+import router from '../router';
 import { useUserStore } from '../stores/user';
 
 const {currentUsername, currentProfilePicture, updateSession} = useUserStore();
@@ -11,6 +12,10 @@ const isChangeUsernameOpen = ref(false);
 const isChangePasswordOpen = ref(false);
 const isDeleteAccountOpen = ref(false)
 const isLogOutOpen = ref(false);
+
+const goBack = () => {
+  router.go(-1);
+};
 
 const showChangeUsername = () => {
   isChangeUsernameOpen.value = true;
@@ -53,15 +58,19 @@ const updateLogOut = () => {
 <template>
     <div class="modal-overlay">
       <div class="modal-content">
-        <h1>Setting</h1>
+        <div class="header-container">
+            <h1>Setting</h1>
+            <button class = "back-btn" @click="goBack">Back</button>
+        </div>
+        <hr>
         <div class = "column">
-            <button @click="showChangeUsername">Change Username</button>
+            <button class = "btn" @click="showChangeUsername">Change Username</button>
             <ChangeUsername :isOpen="isChangeUsernameOpen" :currentUsername="currentUsername" @close="updateUsername"/>
-            <button @click="showChangePassword">Change Password</button>
+            <button class = "btn"  @click="showChangePassword">Change Password</button>
             <ChangePassword :isOpen="isChangePasswordOpen" @close="updatePassword"/>
-            <button @click="showDeleteAccount">Delete Account</button>
+            <button  class = "btn" @click="showDeleteAccount">Delete Account</button>
             <DeleteAccount :isOpen="isDeleteAccountOpen" @close = "updateDeleteAccount" />
-            <button @click="showLogOut">Logout</button>
+            <button  class = "btn" @click="showLogOut">Logout</button>
             <LogOut :isOpen="isLogOutOpen" @close = "updateLogOut" />
         </div>
         
@@ -71,6 +80,43 @@ const updateLogOut = () => {
 </template>
 
 <style scoped>
+.header-container {
+  display: flex;
+  align-items: center; /* Vertically align items in the middle */
+  justify-content: space-between; /* Space items out between their container */
+  width: 100%; /* Take full width of its parent */
+}
+.back-btn{
+    background-color: transparent;
+    border: none;
+    color: var(--dark-purple);
+    font-size: 2.5vh;
+    padding: 1vh;
+    font-weight: 600;
+    padding-right: 5%;
+    padding-left: 5%;
+}
+
+
+.back-btn:hover{
+    background-color: var(--blue-gray);
+    color: var(--deep-purple);
+}
+.btn{
+    background-color: transparent;
+    border: none;
+    color: var(--dark-purple);
+    font-size: 2.5vh;
+    padding: 1vh;
+    font-weight: 600;
+}
+
+.btn:hover{
+    background-color: var(--blue-gray);
+    width: 100%;
+    color: var(--deep-purple);
+}
+
 .modal-overlay {
   position: fixed;
   top: 0;
