1bd7eb8ded85dd9f1bf8e73b75b54d1b38842f9d
Angelina Zheng <ajz@mit.edu>
2023-12-06 02:01:50 -0500

    Route login and register to homepage

--- client/components/Login/LoginForm.vue
+++ client/components/Login/LoginForm.vue
@@ -10,8 +10,7 @@ const { loginUser, updateSession } = useUserStore();
 async function login() {
   await loginUser(username.value, password.value);
   void updateSession();
-  // void router.push({ name: "Home" });
-  void router.push({ name: "Task" });
+  void router.push({ name: "Home" });
 }
 </script>
 
